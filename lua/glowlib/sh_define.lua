e82cfe791bbb49a231846aea53af88b6dd73e239
eon (bloodycop) <minerva.servers.eon@gmail.com>
2024-07-01 01:41:15 +0300

    Update sh_define.lua

--- lua/glowlib/sh_define.lua
+++ lua/glowlib/sh_define.lua
@@ -67,69 +67,15 @@ GlowLib:Define("models/vortigaunt.mdl", {
     ColorAlpha = 255,
 })
 
-
-
-GlowLib:Define("models/dpfilms/metropolice/hl2concept.mdl", {
+GlowLib:Define("models/dog.mdl", {
     Position = function(self, ent)
         local attachmentData = ent:GetAttachment(ent:LookupAttachment("eyes"))
-        return attachmentData.Pos + attachmentData.Ang:Forward() * 1.55
+        return attachmentData.Pos + attachmentData.Ang:Forward() * 0
     end,
     Attachment = "eyes",
-    Size = 0.2,
+    Size = 0.25,
     Color = {
-        [0] = Color(0, 175, 255),
-    },
-    ColorAlpha = 200,
-})
-
-GlowLib:Define("models/dpfilms/metropolice/civil_medic.mdl", {
-    Position = function(self, ent)
-        local attachmentData = ent:GetAttachment(ent:LookupAttachment("eyes"))
-        return attachmentData.Pos + attachmentData.Ang:Forward() * 1.55
-    end,
-    Attachment = "eyes",
-    Size = 0.2,
-    Color = {
-        [0] = Color(255, 230, 0),
-    },
-    ColorAlpha = 200,
-})
-
-GlowLib:Define("models/dpfilms/metropolice/elite_police.mdl", {
-    Position = function(self, ent)
-        local attachmentData = ent:GetAttachment(ent:LookupAttachment("eyes"))
-        return attachmentData.Pos + attachmentData.Ang:Forward() * 1.55 - attachmentData.Ang:Right() * 0
-    end,
-    Attachment = "eyes",
-    Size = 0.2,
-    Color = {
-        [0] = Color(255, 230, 0),
-    },
-    ColorAlpha = 200,
-})
-
-GlowLib:Define("models/dpfilms/metropolice/phoenix_police.mdl", {
-    Position = function(self, ent)
-        local attachmentData = ent:GetAttachment(ent:LookupAttachment("eyes"))
-        return attachmentData.Pos + attachmentData.Ang:Forward() * 1.55
-    end,
-    Attachment = "eyes",
-    Size = 0.2,
-    Color = {
-        [0] = Color(255, 0, 0),
-    },
-    ColorAlpha = 255,
-})
-
-GlowLib:Define("models/dpfilms/metropolice/police_bt.mdl", {
-    Position = function(self, ent)
-        local attachmentData = ent:GetAttachment(ent:LookupAttachment("eyes"))
-        return attachmentData.Pos + attachmentData.Ang:Forward() * 1.55
-    end,
-    Attachment = "eyes",
-    Size = 0.2,
-    Color = {
-        [0] = Color(255, 0, 0),
+        [0] = Color(255, 50, 0),
     },
     ColorAlpha = 255,
 })
