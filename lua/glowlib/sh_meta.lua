8798ff07e82d10a5a33e8fb7ad0e10415d9eef20
eon (bloodycop) <minerva.servers.eon@gmail.com>
2024-09-24 23:18:11 +0300

    Update sh_meta.lua

--- lua/glowlib/sh_meta.lua
+++ lua/glowlib/sh_meta.lua
@@ -4,6 +4,8 @@ function ENTITY:GetGlowingEyes()
     local eyes = {}
 
     for k, v in ipairs(self:GetChildren()) do
+        if ( !IsValid(v) ) then continue end
+
         if ( v:GetNW2String("GlowEyeName", "") == "GlowLib_Eye_" .. self:EntIndex() ) then
             table.insert(eyes, v)
         end
